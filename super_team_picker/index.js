7a7284c757648d1fb65cb342f73ca1f8659194cb
Xiaojun Lin <lin.summer@outlook.com>
2022-07-01 17:15:56 +0000

    tweaks

--- super_team_picker/index.js
+++ super_team_picker/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.join(__dirname, '/views'));
 
 
 
